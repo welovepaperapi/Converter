c9d8d1bb9e38b927bcb6b519472c0a7e28e20026
WeLovePaperAPI <149483567+welovepaperapi@users.noreply.github.com>
2025-10-29 10:19:36 +0100

    Add Java to C++ file conversion functionality
    Implement file conversion from Java to C++ using a lexer and code generator.

--- JavatoCppConverter.cpp
+++ JavatoCppConverter.cpp
@@ -1,1 +1,31 @@
+#include "JavaToCppConverter.h"
+#include "Lexer.h"
+#include "AST.h"
+#include "CodeGenerator.h"
+#include <fstream>
+#include <sstream>
 
+std::string JavaToCppConverter::convertFile(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (!file) return "// Error: Could not open file\n";
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    std::string source = buffer.str();
+
+    Lexer lexer(source);
+    auto tokens = lexer.tokenize();
+
+    ClassNode cls;
+    for (size_t i = 0; i < tokens.size(); ++i) {
+        if (tokens[i].type == TokenType::Keyword && tokens[i].value == "class") {
+            if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::Identifier) {
+                cls.name = tokens[i + 1].value;
+                break;
+            }
+        }
+    }
+
+    CodeGenerator generator;
+    return generator.generate(&cls);
+}

1d6f2fc346f30cfcb736172f590376b5c57dcfe3
WeLovePaperAPI <149483567+welovepaperapi@users.noreply.github.com>
2025-10-29 10:18:21 +0100

    Implement code generation for class definition

--- CodeGenerator.cpp
+++ CodeGenerator.cpp
@@ -1,1 +1,9 @@
+#include "CodeGenerator.h"
+#include <sstream>
 
+std::string CodeGenerator::generate(const ClassNode* cls) {
+    std::ostringstream oss;
+    oss << "#include <iostream>\n\n";
+    oss << "class " << cls->name << " {\npublic:\n    // Methods go here\n};\n";
+    return oss.str();
+}

564e169fcead5b3aa9aa21b971797c120e7ebbdb
WeLovePaperAPI <149483567+welovepaperapi@users.noreply.github.com>
2025-10-29 10:16:27 +0100

    Add ASTNode and ClassNode structures

--- AST.h
+++ AST.h
@@ -1,1 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
 
+struct ASTNode {
+    virtual ~ASTNode() = default;
+};
+
+struct ClassNode : ASTNode {
+    std::string name;
+};

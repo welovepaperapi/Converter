6fa53f7a491fbba4f3a46c83b1bcb5e9dac4bbcb
WeLovePaperAPI <149483567+welovepaperapi@users.noreply.github.com>
2025-10-29 10:15:20 +0100

    Add Lexer class and tokenization structure

--- Lexer.h
+++ Lexer.h
@@ -1,1 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
 
+enum class TokenType { Keyword, Identifier, Symbol, Literal };
+
+struct Token {
+    TokenType type;
+    std::string value;
+    int line;
+};
+
+class Lexer {
+public:
+    Lexer(const std::string& src);
+    std::vector<Token> tokenize();
+
+private:
+    std::string source;
+    size_t pos;
+    int line;
+
+    bool isKeyword(const std::string& word);
+};
